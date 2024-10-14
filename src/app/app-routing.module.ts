32f7816e2d6f3d1154d83d40e930d3354992b0c3
NicolasPS <nifasapi45@gmail.com>
2024-10-14 20:49:43 -0300

    AuthApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { isAuthenticatedGuard, isNotAuthenticatedGuard } from './auth/guards';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    canActivate: [ isNotAuthenticatedGuard ],
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    canActivate: [ isAuthenticatedGuard ],
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'authApp';
+
+  private authService = inject(AuthService);
+  private router = inject(Router)
+
+  public finishedAuthCheck = computed<boolean>( () => {
+
+    if(this.authService.authStatus() === 'checking') return false;
+
+    return true;
+
+  });
+
+  public authStatusChangedEffect = effect( () => {
+
+    switch(this.authService.authStatus()){
+      case 'authenticated':
+        this.router.navigateByUrl(localStorage.getItem('url') || '/dashboard');
+        return;
+      case 'notAuthenticated':
+        this.router.navigateByUrl('/auth/login');
+        return;
+    }
+
+  });
+
 }
